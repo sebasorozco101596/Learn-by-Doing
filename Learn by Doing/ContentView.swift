0368946974fdd6231b51d86b8837b44a0bf1613c
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-02-21 23:30:44 -0500

    Initial Commit

---
+++ Learn by Doing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learn by Doing
+//
+//  Created by Juan Sebastian Orozco Buitrago on 2/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Juan Sebastian Orozco Buitrago on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
